2bbfdd6e42ab92d0f3d485408f34f11bf37e77b3
dirk <dirk@0d842271-ca0e-0410-b7f0-83f617a2c6fd>
2008-08-27 14:07:56 +0000

    Fehlende Klasse Helfer nachgereicht
    git-svn-id: http://wikiserver/svn/tthelper/trunk@369 0d842271-ca0e-0410-b7f0-83f617a2c6fd

---
+++ src/main/java/de/dirent/tthelper/entities/Helfer.java
@@ -0,0 +1,210 @@
+package de.dirent.tthelper.entities;
+
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import org.apache.tapestry.beaneditor.NonVisual;
+import org.apache.tapestry.beaneditor.Validate;
+
+import de.dirent.tthelper.model.TShirtSize;
+import de.dirent.tthelper.model.Verein;
+
+
+@Entity
+public class Helfer 
+
+		extends AbstractEntity implements Cloneable {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -471822420963322708L;
+
+	
+	private Date date = new Date();
+	
+	private Verein verein;
+	private String name;
+	private String anschrift;
+	private String telefonnummer;
+	private Date birthDate;
+	private String email;
+	private TShirtSize size = TShirtSize.XL;
+	private boolean kannDo = true;
+	private boolean kannFr = true;
+	private boolean kannSa = true;
+	private boolean kannSo = true;
+	private boolean kannMo = true;
+	
+	private String bemerkung;
+	
+	
+	@NonVisual
+	public Date getDate() {
+	
+		return date;
+	}
+	public void setDate(Date date) {
+	
+		this.date = date;
+	}
+
+	
+	@Validate( "required" )
+	public Verein getVerein() {
+	
+		return verein;
+	}
+	public void setVerein(Verein verein) {
+	
+		this.verein = verein;
+	}
+	
+	
+	@Validate( "required" )
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	@Validate( "required" )
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	
+	
+	@Validate( "required,maxlength=256" )	
+	public String getAnschrift() {
+	
+		return anschrift;
+	}
+	public void setAnschrift(String anschrift) {
+	
+		this.anschrift = anschrift;
+	}
+	
+
+	@Validate( "required" )
+	public String getTelefonnummer() {
+	
+		return telefonnummer;
+	}
+	public void setTelefonnummer(String telefonnummer) {
+	
+		this.telefonnummer = telefonnummer;
+	}
+
+	
+	@Validate( "maxlength=32" )
+	public String getEmail() {
+	
+		return email;
+	}
+	public void setEmail(String email) {
+	
+		this.email = email;
+	}
+
+	
+	@Validate( "required" )
+	public TShirtSize getSize() {
+	
+		return size;
+	}	
+	public void setSize(TShirtSize size) {
+	
+		this.size = size;
+	}
+
+	
+	public boolean isKannDo() {
+	
+		return kannDo;
+	}
+	
+	public void setKannDo(boolean kannDo) {
+	
+		this.kannDo = kannDo;
+	}
+	
+	public boolean isKannFr() {
+	
+		return kannFr;
+	}
+	
+	public void setKannFr(boolean kannFr) {
+	
+		this.kannFr = kannFr;
+	}
+	
+	public boolean isKannSa() {
+	
+		return kannSa;
+	}
+	
+	public void setKannSa(boolean kannSa) {
+	
+		this.kannSa = kannSa;
+	}
+	
+	public boolean isKannSo() {
+	
+		return kannSo;
+	}
+	
+	public void setKannSo(boolean kannSo) {
+	
+		this.kannSo = kannSo;
+	}
+	
+	public boolean isKannMo() {
+	
+		return kannMo;
+	}
+	public void setKannMo(boolean kannMo) {
+	
+		this.kannMo = kannMo;
+	}
+	
+	
+	@Validate( "maxlength=512" )
+	public String getBemerkung() {
+		return bemerkung;
+	}
+	public void setBemerkung(String bemerkung) {
+		this.bemerkung = bemerkung;
+	}
+
+	
+	public Helfer clone() {
+		
+		Helfer clone = new Helfer();
+		clone.setId( getId() );
+		clone.date = date;
+		clone.name = name;
+		clone.birthDate = birthDate;
+		clone.verein = verein;
+		clone.anschrift = anschrift;
+		clone.telefonnummer = telefonnummer;
+		clone.email = email;
+		clone.size = size;
+		clone.kannDo = kannDo;
+		clone.kannFr = kannFr;
+		clone.kannSa = kannSa;
+		clone.kannSo = kannSo;
+		clone.kannMo = kannMo;
+		clone.bemerkung = bemerkung;
+		
+		return clone;
+	}
+}
