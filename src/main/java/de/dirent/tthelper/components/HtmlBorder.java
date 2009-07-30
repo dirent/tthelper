da7902917501126793a907db9d3c5104973637cb
dirk <dirk@0d842271-ca0e-0410-b7f0-83f617a2c6fd>
2009-07-30 15:57:34 +0000

    dynamisches Datum für 'Stand'
    git-svn-id: http://wikiserver/svn/tthelper/trunk@408 0d842271-ca0e-0410-b7f0-83f617a2c6fd

--- src/main/java/de/dirent/tthelper/components/HtmlBorder.java
+++ src/main/java/de/dirent/tthelper/components/HtmlBorder.java
@@ -1,8 +1,16 @@
 package de.dirent.tthelper.components;
 
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SessionState;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.ApplicationGlobals;
 
 import de.dirent.tthelper.pages.Search;
 
@@ -23,5 +31,23 @@ public class HtmlBorder {
     	this.searchPage.startSearch( this.phrase.toString() );
     	
     	return this.searchPage;
+    }
+    
+    @Inject
+    private ApplicationGlobals globals;
+    
+    public String getLastPublishedDate() {
+    	
+		DateFormat df = new SimpleDateFormat( "dd. MMMM yyyy HH:mm 'Uhr'",  Locale.GERMAN );
+		
+		File tempDir = 
+			(File) globals.getServletContext().getAttribute( "javax.servlet.context.tempdir" );
+
+		// first approximation
+		Date lastPublished = new Date( tempDir.lastModified() );
+		
+		// TODO: check the db for some newer changes
+		
+		return df.format( lastPublished );
     }
 }
